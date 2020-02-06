268d6fde1aef2ae2e5e5395728955f93101f65ab
Crying Surrogate <cryingsurrogate@gmail.com>
2020-02-06 06:56:18 +0300

    Fixed line endings

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
-mkdir $HOME/.webhack/
-echo 'WHAK=$HOME/.webhack' >> $HOME/.bashrc
-echo 'PATH=$WHAK:$PATH' >> $HOME/.bashrc
-cp webhack $WHAK
+mkdir $HOME/.webhack
+export WHAK=$HOME/.webhack
+export PATH=$WHAK:$PATH
+cp webhack $WHAK/webhack
 chmod +777 $WHAK/webhack
+cp WebHack.py $WHAK/WebHack.py
+chmod +777 $WHAK/WebHack.py
+
 cd $HOME
+echo "WHAK=$HOME/.webhack" >> .bashrc
+echo "PATH=$WHAK:$PATH" >> .bashrc
+source .bashrc
 pip3 install requests
 pip3 install colorama
-clear
