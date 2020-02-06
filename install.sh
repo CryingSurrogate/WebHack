e3836d52579ad03dd6f8354fb35c481cfc8e28cb
Crying Surrogate <cryingsurrogate@gmail.com>
2020-02-06 06:25:49 +0300

    Нормальная установка

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
-cp webhack /data/data/com.termux/files/usr/bin/
-cd /data/data/com.termux/files/usr/bin/
-chmod +777 webhack
-cd $Home
+mkdir $HOME/.webhack/
+echo 'WHAK=$HOME/.webhack' >> $HOME/.bashrc
+echo 'PATH=$WHAK:$PATH' >> $HOME/.bashrc
+cp webhack $WHAK
+chmod +777 $WHAK/webhack
+cd $HOME
 pip3 install requests
+pip3 install colorama
 clear
